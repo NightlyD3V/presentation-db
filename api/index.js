7e21dacefcf13454df9b4e18e7bbdacc42263ca0
ghost <ghost@pop-os.localdomain>
2023-08-12 23:21:16 -0400

    Initial setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require("cors")
 const app = express()
 const db = require('./db/messages.js')
 const PORT = process.env.PORT || 3000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send("It's working!")
