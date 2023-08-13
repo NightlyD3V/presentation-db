00f1706f64f461e2f3bbb0631703645f906915bd
ghost <ghost@pop-os.localdomain>
2023-08-12 23:00:31 -0400

    Initial setup

--- api/index.js
+++ api/index.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const app = express()
 const db = require('./db/messages.js')
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send("It's working!")
 })
 
-app.get('/api', async (req, res) => {
+app.get('/messages', async (req, res) => {
     const messages = await db.getAllMsg()
     res.status(200).json({ messages })
 })
 
-app.post('/api', async (req, res) => {
+app.post('/messages', async (req, res) => {
     const result = await db.createMsg(req.body)
     res.status(200).json({id: result[0]})
 })
