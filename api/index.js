aed9d4c36cf041a0bfb1ede2fa417600128d6557
ghost <ghost@pop-os.localdomain>
2023-08-12 21:47:26 -0400

    Initial setup

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const db = require('./db/messages')
+const db = require('./db/messages.js')
 
 app.get('/', (req, res) => {
     res.send("It's working!")
