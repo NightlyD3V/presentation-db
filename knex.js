0466ca1b812a6edd62a7a8711d431d3647a2b1b8
ghost <ghost@pop-os.localdomain>
2023-08-13 00:06:40 -0400

    Initial setup

--- knex.js
+++ knex.js
@@ -1,10 +1,9 @@
-const knex = require('knex')
-
-const connectedKnex = knex({
-    client: "sqlite3",
-    connection: {
-        filename: "db.sqlite3"
-    }
-})
-
-module.exports = connectedKnex
+module.exports = {
+    development: {
+      client: 'sqlite3',
+      connection: {
+        filename: 'db.sqlite3', 
+      },
+      useNullAsDefault: true,
+    },
+  };
--- messages.js
+++ messages.js
@@ -1,4 +1,4 @@
-const knex = require('./knex.js')
+const knex = require('./knex')
 
 function createMsg(message){
     return knex('Messages'.insert(message))
